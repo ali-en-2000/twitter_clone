e58e94b04a791758f69eec930df5dfce4d17863e
ali-en-2000 <a.rezamohmmadi369@gmail.com>
2022-04-06 21:22:40 +0430

    add widgets component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Widgets from './Widgets';
 
 import './App.css';
 
 function App() {
   return (
     //BEM
-    <div className="App">
+    <div className="app">
       <Sidebar/>    
 
       {/* feed */}
       <Feed/>
 
       {/* widgets */}
-
+      <Widgets/>
     </div>
   );
 }
